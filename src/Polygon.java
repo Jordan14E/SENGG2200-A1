5cf1d01b52cefc5700b4c5a828aa679fc7b0043d
Jordan14E <92713486+Jordan14E@users.noreply.github.com>
2022-02-28 14:39:24 +1100

    Polygon toString()

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,6 +1,24 @@
 
 public class Polygon {
 
+    Point[] vertices;
+    int numVertices;
+
+    Polygon(int num){
+        numVertices = num;
+        vertices = new Point[num];
+    }
+
+    public String toString(){
+
+        //initialise string
+        String str = "";
+        for(int i=0; i<numVertices; i++){
+            str += vertices[i].toString();
+        }
+
+        return str;
+    }
 
 
 }
